42ccb4782b45962e31c7ff5d5d25a872ec32e07a
Anh Phan Nguyen <phananh1010@yahoo.com>
2019-02-10 16:27:01 -0500

    grammar checking

--- example.py
+++ example.py
@@ -1,16 +1,16 @@
 import pickle
 
 if __name__ == "__main__":
-    #TODO: a simple example to load the saliency dataset file
-    #RETURN: value of the first record of the file. The record includes: timestamp, fixation_list, and saliency_map
+    #TODO: a simple example to load a saliency dataset file
+    #RETURN: the values of the first record of the file. The record includes: timestamp, fixation_list, and saliency_map
     
     #load the dataset file named `saliency_ds1_topicparis` (ds=1, video=paris)
     data = pickle.load(open('./data/saliency_ds1_topicparis'))
     
-    #access first record
+    #access the first record
     timestamp, fixation_list, saliency_map = data[0]
     
-    #print out the values of fields in the record
+    #print out the values of fields in the first record
     print timestamp
     print fixation_list
     print saliency_map
